8d7d8fac281ccbb6e3eb9f08161dc7943fd0d79f
gdh1995 <gdh1995@qq.com>
2015-11-09 22:48:56 +0800

    test shows loading jni modules is OK

--- app/src/main/java/com.example.myapplication.app/CNNWorker.java
+++ app/src/main/java/com.example.myapplication.app/CNNWorker.java
@@ -3,6 +3,9 @@ package com.example.myapplication.app;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
+
+import com.sh1r0.caffe_android_demo.CaffeMobile;
 
 import java.io.File;
 import java.io.IOException;
@@ -36,7 +39,12 @@ public class CNNWorker implements Runnable {
     public static int[] work() {
         int [] result = null;
         try {
+            CaffeMobile caffeMobile = CaffeMobile.Get();
+            caffeMobile.enableLog(false);
+            caffeMobile.enableLog(true);
+            caffeMobile.loadModel("", "");
         } catch (Exception e) {
+            Log.e("CNN Run", e.getMessage());
             e.printStackTrace();
         }
         return result;
